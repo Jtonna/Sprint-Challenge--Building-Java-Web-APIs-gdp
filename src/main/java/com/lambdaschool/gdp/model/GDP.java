e7d7f1232152a470bbba82391beaecd273ca5dae
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-07 12:02:50 -0700

    Finished GDP Model

--- src/main/java/com/lambdaschool/gdp/model/GDP.java
+++ src/main/java/com/lambdaschool/gdp/model/GDP.java
@@ -1,11 +1,15 @@
 package com.lambdaschool.gdp.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 public class GDP {
 
+    private static final Logger logger = new LoggerFactory.getLogger(GDP.class);
     //setting up the data types
-    public static final AtomicLong counter = new AtomicLong();
+    private static final AtomicLong counter = new AtomicLong();
     private long id;
     private String name;
     private String gdp;
@@ -14,6 +18,9 @@ public class GDP {
         this.id = counter.incrementAndGet();
         this.name = name;
         this.gdp = gdp;
+
+        logger.info("We created a country named: " + this.name );
+        logger.debug("Im 99.9999999% sure we created a country with the the id of " + this.id );
     }
 
     public GDP(GDP toClone) {
@@ -22,5 +29,32 @@ public class GDP {
         this.gdp = toClone.getGdp();
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(String gdp) {
+        this.gdp = gdp;
+    }
+
+    @Override
+    public String toString() {
+        return "GDP{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gdp='" + gdp + '\'' +
+                '}';
+    }
 }
