9862e88deeab81b0790421640e63accdaede3225
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-07 11:20:18 -0700

    Working on the Model

--- src/main/java/com/lambdaschool/gdp/model/GDP.java
+++ src/main/java/com/lambdaschool/gdp/model/GDP.java
@@ -1,5 +1,17 @@
 package com.lambdaschool.gdp.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 public class GDP {
+
+    // private static final Logger logger = LoggerFactory.getLogger(GDP.class);
+    //setting up the data types
+    public static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String name;
+    private String gdp;
 
 }
